9ecf15e41415317d1a0f05f660de5171c0dcdfce
梶原耕平 <kajiharakouhei@kajiwarakouheinoMacBook-Air.local>
2021-07-02 17:37:10 +0900

    20210702

--- database/migrations/2021_06_30_092937_create_todo_table.php
+++ database/migrations/2021_06_30_092937_create_todo_table.php
@@ -15,9 +15,10 @@ class CreateTodoTable extends Migration
     {
         Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            
             $table->timestamp('created_at')->useCurrent()->nullable();
             $table->timestamp('updated_at')->useCurrent()->nullable();
+            $table->text('content');
         });
     }
 
