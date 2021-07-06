02219d2f2c29c2621832ef7d4a4cf8d47e19875d
梶原耕平 <kajiharakouhei@kajiwarakouheinoMacBook-Air.local>
2021-07-06 18:21:09 +0900

    20210706

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,11 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable=['content','created_at','updated_at'];
+    protected $fillable=['content'];
     public static $rules = array(
         'content'=>'required|min:0|max:20',
-        'created_at'=>'required|min:0|max:20',
-        'updated_at'=>'required|min:0|max:20',
+       
     );
     public function getData(){
         return $this->text. ':' . $this->created_at. '(' . $this->updated_at . ')';
