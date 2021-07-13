159dc0b2f590a8d93b4ee638f507dca953157491
梶原耕平 <kajiharakouhei@kajiwarakouheinoMacBook-Air.local>
2021-07-13 11:29:43 +0900

    20210713

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -31,6 +31,6 @@ class TodoController extends Controller
     public function delete(Request $request)
     {
         $todo = Todo::find($request->id);
-        return view('/', ['form' => $todo]);
+        return redirect('/');
     }
 }
